14d992fce32124d8016e8ed1343de43043da7ada
Luke <luke.stephens@trintel.co.za>
2024-03-20 17:06:07 +0200

    fixes

--- src/app/writing/[...slug]/layout.tsx
+++ src/app/writing/[...slug]/layout.tsx
@@ -1,9 +1,9 @@
-import {ReactElement} from "react";
+import React from "react";
 
-export default function Layout({children} : {children: ReactElement}) {
-  return (
-    <div className={`mt-4`}>
+export default function Layout({children}: { children: React.ReactNode }) {
+	return (
+		<div className={`mt-4`}>
 			{children}
-    </div>
-  );
+		</div>
+	);
 };
