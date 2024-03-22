d23b6e5564810b5a520b546d46d21a65c3101ec8
Luke <luke.stephens@trintel.co.za>
2024-03-22 14:04:46 +0200

    location

--- src/components/tech.tsx
+++ src/components/tech.tsx
@@ -9,9 +9,9 @@ export default function Tech() {
 				<SmallLink title={"NextJs"} link={"https://nextjs.org/"}/>
 				<SmallLink title={"Django"} link={"https://www.djangoproject.com/"}/>
 				<SmallLink title={"Supabase"} link={"https://supabase.com"}/>
+				<SmallLink title={"Firebase"} link={"https://firebase.google.com/"}/>
 				<SmallLink title={"React Native"} link={"https://reactnative.dev/"}/>
 				<SmallLink title={"Flutter"} link={"https://flutter.dev/"}/>
-				<SmallLink title={"Firebase"} link={"https://firebase.google.com/"}/>
 				<SmallLink title={"MongoDB"} link={"https://www.mongodb.com/"}/>
 				<SmallLink title={"Postgres"} link={"https://www.postgresql.org/"}/>
 				<SmallLink title={"OpenAI"} link={"https://openai.com/"}/>
