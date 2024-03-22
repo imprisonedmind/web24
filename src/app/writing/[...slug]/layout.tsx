94fc4e00ec24b57d6d1732584335534b884c267f
Luke <luke.stephens@trintel.co.za>
2024-03-22 18:00:44 +0200

    add open graph

--- src/app/writing/[...slug]/layout.tsx
+++ src/app/writing/[...slug]/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Layout({children}: { children: React.ReactNode }) {
 	return (
-		<div className={`mt-4`}>
+		<div className={`my-4`}>
 			{children}
 		</div>
 	);
