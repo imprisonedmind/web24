4fbb983189d75f9a0b402f2b994a627e9d606684
Luke <luke.stephens@trintel.co.za>
2024-05-01 00:11:23 +0200

    add github

--- src/components/social.tsx
+++ src/components/social.tsx
@@ -6,9 +6,10 @@ export default function Social() {
 		<div>
 			<Header title={"social"}/>
 			<div className={"flex flex-col"}>
+				<SmallLink title={"thecrag.com"} link={"https://www.thecrag.com/climber/luke6"}/>
 				<SmallLink title={"twitter.com"} link={"https://twitter.com/lukey_stephens"}/>
 				<SmallLink title={"layers.to"} link={"https://layers.to/lukey"}/>
-				<SmallLink title={"thecrag.com"} link={"https://www.thecrag.com/climber/luke6"}/>
+				<SmallLink title={"github.com"} link={"https://github.com/imprisonedmind"}/>
 			</div>
 		</div>
 	);
