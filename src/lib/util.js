344886fc40b38a153091a879f0a26bfc13516f5f
Luke <luke.stephens@trintel.co.za>
2024-08-19 10:01:06 +0200

    update

--- src/lib/util.js
+++ src/lib/util.js
@@ -54,8 +54,16 @@ export const returnSongData = async () => {
   }
 
   const song = await response.json();
+
+  if (song.currently_playing_type === "episode") {
+    return {
+      status: 200,
+      json: { isPlaying: false },
+    };
+  }
+
   const isPlaying = song.is_playing;
-  const title = song.item.name;
+  const title = song.item.name || "unkown";
   const artist = song.item.artists.map((_artist) => _artist.name).join(", ");
   const album = song.item.album.name;
   const albumImageUrl = song.item.album.images[0].url;
