8de324ad57b16bdb8f6a488fc3b1337f34414c16
Luke <luke.stephens@trintel.co.za>
2024-10-18 09:22:22 +0200

    update link

--- src/components/smallLink.tsx
+++ src/components/smallLink.tsx
@@ -4,13 +4,19 @@ import Link from "next/link";
 interface SmallLinkProps {
   title: string;
   link: string;
+  target?: boolean;
 }
 
-export const SmallLink: FC<SmallLinkProps> = ({ title, link }) => {
+export const SmallLink: FC<SmallLinkProps> = ({
+  title,
+  link,
+  target = false,
+}) => {
   return (
     <Link
       prefetch={true}
       href={link}
+      target={target ? "_blank" : "_self"}
       className="
         flex w-max text-sm text-neutral-500 underline-offset-4 hover:underline
       "
