6a3cb85929b63463c64a3d51dd933eaa8bbe8296
Luke <luke.stephens@trintel.co.za>
2024-10-18 09:22:22 +0200

    update link

--- src/components/social.tsx
+++ src/components/social.tsx
@@ -1,16 +1,32 @@
-import {Header} from "@/components/header";
-import {SmallLink} from "@/components/smallLink";
+import { Header } from "@/components/header";
+import { SmallLink } from "@/components/smallLink";
 
 export default function Social() {
-	return (
-		<div>
-			<Header title={"social"}/>
-			<div className={"flex flex-col"}>
-				<SmallLink title={"thecrag.com"} link={"https://www.thecrag.com/climber/luke6"}/>
-				<SmallLink title={"twitter.com"} link={"https://twitter.com/lukey_stephens"}/>
-				<SmallLink title={"layers.to"} link={"https://layers.to/lukey"}/>
-				<SmallLink title={"github.com"} link={"https://github.com/imprisonedmind"}/>
-			</div>
-		</div>
-	);
-};
+  return (
+    <div>
+      <Header title={"social"} />
+      <div className={"flex flex-col"}>
+        <SmallLink
+          target={true}
+          title={"thecrag.com"}
+          link={"https://www.thecrag.com/climber/luke6"}
+        />
+        <SmallLink
+          target={true}
+          title={"twitter.com"}
+          link={"https://twitter.com/lukey_stephens"}
+        />
+        <SmallLink
+          target={true}
+          title={"layers.to"}
+          link={"https://layers.to/lukey"}
+        />
+        <SmallLink
+          target={true}
+          title={"github.com"}
+          link={"https://github.com/imprisonedmind"}
+        />
+      </div>
+    </div>
+  );
+}
