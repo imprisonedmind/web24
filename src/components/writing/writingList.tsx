fc0940f9746f1c893b58226ca984ae0bef6df058
imprisonedmind <imahowiie@gmail.com>
2025-11-05 16:57:51 +0200

    feat(writing): add hover previews and refresh metadata

--- src/components/writing/writingList.tsx
+++ src/components/writing/writingList.tsx
@@ -1,10 +1,9 @@
 import { Header } from "@/components/header";
-import { SmallLink } from "@/components/smallLink";
 import blogData from "@/lib/blogData.json";
 import reviewData from "@/lib/reviewData.json";
-import { getTopThreePosts, spaceToHyphen } from "@/lib/util";
+import { getTopThreePosts } from "@/lib/util";
 import { Post } from "@/lib/types";
-import { ReviewScore } from "@/components/writing/reviewScore";
+import { WritingPreviewLink } from "@/components/writing/writingPreviewLink";
 
 export default function WritingList() {
   const data = [...blogData, ...reviewData];
@@ -13,14 +12,8 @@ export default function WritingList() {
     <div className={"-mt-4 flex w-full flex-col gap-1 px-4 md:mt-0 md:px-0"}>
       <Header title={"writing"} seeAll={true} link={"writing"} />
 
-      {getTopThreePosts(data).map((item: Post, index: number) => (
-        <span key={index} className="flex items-center justify-between">
-          <SmallLink
-            title={item.title}
-            link={`/writing/${spaceToHyphen(item.title)}/${item.id}`}
-          />
-          {item.score !== undefined && <ReviewScore score={item.score} />}
-        </span>
+      {getTopThreePosts(data).map((item: Post) => (
+        <WritingPreviewLink key={item.id} item={item} />
       ))}
     </div>
   );
